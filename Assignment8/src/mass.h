a05f09bf62fb15fa82b116cde771e07d7f7f750a
2095 <lpk7777@outlook.com>
2026-01-04 20:04:21 +0800

    作业1-7的结果+作业8布置。

--- Assignment8/src/mass.h
+++ Assignment8/src/mass.h
@@ -7,24 +7,25 @@
 using namespace CGL;
 
 struct Mass {
-  Mass(Vector2D position, float mass, bool pinned)
-      : start_position(position), position(position), last_position(position),
-        mass(mass), pinned(pinned) {}
+	Mass(Vector2D position, float mass, bool pinned)
+		: start_position(position), position(position), last_position(position),
+		  mass(mass), pinned(pinned) {
+	}
 
-  float mass;
-  bool pinned;
+	float mass;
+	bool pinned;
 
-  Vector2D start_position;
-  Vector2D position;
+	Vector2D start_position;
+	Vector2D position;
 
-  // explicit Verlet integration
+	// explicit Verlet integration
 
-  Vector2D last_position;
+	Vector2D last_position;
 
-  // explicit Euler integration
+	// explicit Euler integration
 
-  Vector2D velocity;
-  Vector2D forces;
+	Vector2D velocity;
+	Vector2D forces;
 };
 
 #endif /* MASS_H */
--- Assignment8/src/spring.h
+++ Assignment8/src/spring.h
@@ -7,16 +7,16 @@
 using namespace std;
 
 namespace CGL {
+	struct Spring {
+		Spring(Mass* a, Mass* b, float k)
+			: m1(a), m2(b), k(k), rest_length((a->position - b->position).norm()) {
+		}
 
-struct Spring {
-  Spring(Mass *a, Mass *b, float k)
-      : m1(a), m2(b), k(k), rest_length((a->position - b->position).norm()) {}
+		float k;
+		double rest_length;
 
-  float k;
-  double rest_length;
-
-  Mass *m1;
-  Mass *m2;
-}; // struct Spring
+		Mass* m1;
+		Mass* m2;
+	}; // struct Spring
 }
 #endif /* SPRING_H */
